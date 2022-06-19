2a24745bffeabd1765b1de5845bbab7ef3dbfaef
AidenFrost-beep <Duvann0@gmail.com>
2022-06-18 22:00:31 -0400

    ahora se pueden agregar datos a la base de datos
    falta la revision sobre la agregacion de imagenes al igual que el leer actualizar y eliminar sin embargo el inicio ya esta completo

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        //Creamos la conexion
+        $conn = new mysqli($servername, $username, $password);
+
+        //verificamos la conexion
+        if ($conn->connect_error){
+              die("Conexion fallida: " . $conn->connect_error);
+        }else{
+        echo "Conexion establecida...";
+    }
+
+?>
