9c59761bc56f074b93cd0a79793087b6ef98fac0
araeri <aracely.leona1@gmail.com>
2022-06-19 16:27:53 -0400

    id arreglado
    fefewfew

--- Proyecto/enviar.php
+++ Proyecto/enviar.php
@@ -4,8 +4,8 @@ include "conexion.php";
 
 
 
-$sql = "INSERT INTO sistemadeinfomacion.producto (id,nombre,imagen,precio,categoria,temporada,fechaingreso,descripcion)
-VALUES ('$_GET[id]','$_GET[nombre]','$_GET[imagen]', '$_GET[precio]','$_GET[categoria]','$_GET[temporada]'
+$sql = "INSERT INTO sistemadeinfomacion.producto (nombre,imagen,precio,categoria,temporada,fechaingreso,descripcion)
+VALUES ('$_GET[nombre]','$_GET[imagen]', '$_GET[precio]','$_GET[categoria]','$_GET[temporada]'
 ,'$_GET[fechaingreso]','$_GET[descripcion]')";
 
  echo "XD";
